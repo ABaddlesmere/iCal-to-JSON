8775753a94c4b6380db4a57d6fc103506e2d25c8
ABaddlesmere <60923666+ABaddlesmere@users.noreply.github.com>
2022-07-06 10:29:19 +0100

    Remove unused comment

--- example.py
+++ example.py
@@ -3,7 +3,6 @@ from iCalToJson import iCal
 #Some raw data
 DATA = "BEGIN:VCALENDAR\nX-WR-CALNAME:Example\nX-WR-TIMEZONE:Europe/London\nPRODID:-//Example PRODID v2021//EN\nVERSION:2.0\nBEGIN:VEVENT\nSUMMARY:This is the summary for event1\nLOCATION:This is the location for event1\nDTSTAMP:20210610T110000Z\nDTSTART:20210610T110000\nDTEND:20210610T180000\nCLASS:PUBLIC\nDESCRIPTION:This is the description for event1\nUID:00000000000000000000000000000001\nEND:VEVENT\nBEGIN:VEVENT\nSUMMARY:This is the summary for event2\nLOCATION:This is the location for event2\nDTSTAMP:20210710T110000Z\nDTSTART:20210710T110000\nDTEND:20210710T180000\nCLASS:PUBLIC\nDESCRIPTION:This is the description for event2\nUID:00000000000000000000000000000002\nEND:VEVENT"
 
-#The settings needed for our converter. Leaving these parameters empty will use defaults outlined in the README documentation
 
 #Create the converter using our settings
 converter = iCal(autoReadableDate=True)
